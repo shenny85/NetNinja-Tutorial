b07a74abcc8a63e2b47d4f53d7ebae28e67bb981
shenny85 <shenderson144@ivytech.edu>
2023-12-06 02:11:11 -0500

    Tutorial #11

--- node-crash-course-lesson-9/app.js
+++ node-crash-course-lesson-9/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const blogRoutes = require('./Routes/blogRoutes');
+
 
 // express app
 const app = express();
 
 // connect to mongodb & listen for requests
-const dbURI = 'mongodb+srv://shenny:test2325@saavycoder.wiry4bc.mongodb.net/node-tutorial?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://shenny:test2325@saavycoder.wiry4bc.mongodb.net/node-tutorial/blogs';
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => app.listen(3001))
@@ -69,55 +70,10 @@ app.get('/about', (req, res) => {
 
 
 // blog routes
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
-});
-
-app.get('/blogs', (req, res) => {
-  Blog.find().sort({ createdAt: -1 })
-    .then(result => {
-      res.render('index', { blogs: result, title: 'All blogs' });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-app.post('/blogs', (req, res) => {
-  const blog = new Blog(req.body);
-  blog.save()
-  .then(result => {
-    res.redirect('/blogs');
-  })
-  .catch(err => {
-    console.log(err);
-  });
+app.use(blogRoutes)
 
 
-})
 
-app.get('/blogs/:id', (req, res) => {
-  const id = req.params.id;
- Blog.findById(id)
- .then(result => {
-  res.render('details', { blog: result, title: 'Blog Details' });
-})
-.catch(err => {
-  console.log(err);
-});
-});
-
-app.delete('/blogs/:id', (req, res) => {
-  const id = req.params.id;
-
-  Blog.findByIdAndDelete(id)
-  .then(result => {
-  res.json({redirect: '/blogs'}) 
-  })
-  .catch(err => {
-    console.log(err);
-  });
-})
 
 // 404 page
 app.use((req, res) => {
